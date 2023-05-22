b9e61a29abc1d9b443f19e412adc4290f4902ef8
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-05-22 15:54:39 +0200

    Enh: Error returning access function definitions

--- access.go
+++ access.go
@@ -2,6 +2,10 @@ package pimit
 
 import "image/color"
 
+type accessFuncConstraint interface {
+	ReadAccess | ReadColorAccess | ReadWriteAccess | ReadWriteColorAccess | ReadAccessE | ReadColorAccessE | ReadWriteAccessE | ReadWriteColorAccessE
+}
+
 // Iteration access function. Allows you to read the coordinates of a pixel and its color.
 type ReadAccess = func(xIndex, yIndex int, color color.Color)
 
@@ -13,3 +17,15 @@ type ReadWriteAccess = func(xIndex, yIndex int, color color.Color) color.Color
 
 // Iteration access function. Allows you to read the pixel color. Returns the color to be set.
 type ReadWriteColorAccess = func(color color.Color) color.Color
+
+// Iteration access function. Allows you to read the coordinates of a pixel and its color. Errors can be passed down to the iterator.
+type ReadAccessE = func(xIndex, yIndex int, color color.Color) error
+
+// Iteration access function. Allows you to read the pixel color. Errors can be passed down to the iterator.
+type ReadColorAccessE = func(color color.Color) error
+
+// Iteration access function. Allows you to read the coordinates of a pixel and its color. Returns the color to be set. Errors can be passed down to the iterator.
+type ReadWriteAccessE = func(xIndex, yIndex int, color color.Color) (color.Color, error)
+
+// Iteration access function. Allows you to read the pixel color. Returns the color to be set. Errors can be passed down to the iterator.
+type ReadWriteColorAccessE = func(color color.Color) (color.Color, error)
