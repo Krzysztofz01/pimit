7ee9198197219186992cddf5ff23a467e915ae91
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2023-09-16 15:01:19 +0200

    Enh: ErrorTrap utility implementation

---
+++ error.go
@@ -0,0 +1,31 @@
+package pimit
+
+import "sync"
+
+type errorTrap struct {
+	err error
+	mu  sync.Mutex
+}
+
+func NewErrorTrap() *errorTrap {
+	return &errorTrap{
+		err: nil,
+		mu:  sync.Mutex{},
+	}
+}
+
+func (et *errorTrap) Set(err error) {
+	et.mu.Lock()
+	defer et.mu.Unlock()
+
+	if err != nil && et.err == nil {
+		et.err = err
+	}
+}
+
+func (et *errorTrap) Err() error {
+	et.mu.Lock()
+	defer et.mu.Unlock()
+
+	return et.err
+}
